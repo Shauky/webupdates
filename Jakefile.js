d98ef5d30d904de9df68ca8f846b1c6205556858
Shauky <shaukathmohamed@gmail.com>
2014-08-07 15:38:39 +0800

    Have basic linting,tests running

---
+++ Jakefile.js
@@ -0,0 +1,59 @@
+/*global desc, task, jake, fail, complete */
+
+(function() {
+
+	"use strict";
+
+	task('default', ["lint","test"]);
+
+	desc("Lints eveything");
+	task("lint", [], function(){
+	    var lint = require("./build/lint/lint_runner.js");
+
+	 	var files = new jake.FileList();
+	 	files.include("**/*.js");
+	 	files.exclude("node_modules");
+	 	var options = nodeLintOptions();
+	 	var globals = nodeLintGlobals();
+		var pass = lint.validateFileList(files.toArray(), options, globals);
+				if (!pass) fail("Lint failed");
+
+	});
+
+	desc("Test everything");
+	task("test", [], function(){
+		var reporter = require("nodeunit").reporters.default;
+		reporter.run(['/']);
+	});
+
+
+	function nodeLintOptions(){
+	    return {
+			bitwise: true,
+			curly: false,
+			eqeqeq: true,
+			forin: true,
+			immed: true,
+			latedef: true,
+			newcap: true,
+			noarg: true,
+			noempty: true,
+			nonew: true,
+			regexp: true,
+			undef: true,
+			strict: true,
+			trailing: true,
+			node: true
+		};
+	}
+
+	function nodeLintGlobals(){
+		return {
+			describe: false,
+			it: false,
+			beforeEach: false,
+			afterEach: false
+		};
+	}
+
+}());
